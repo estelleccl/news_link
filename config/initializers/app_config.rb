ba91bfadebdedcb6dcef234676d06c1079fbe7c9
Esu Chang <Esu@Esus-M.local>
2015-01-13 12:14:10 +0800

    added the gem for fb omniauth for fb

---
+++ config/initializers/app_config.rb
@@ -0,0 +1,8 @@
+require 'yaml'
+
+unless Rails.env.production?
+	yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'application.yml'))).result)
+	APP_CONFIG = HashWithIndifferentAccess.new(yaml_data)
+  ENV['FB_API_KEY'] = APP_CONFIG[:api_key]
+  ENV['FB_SECRET_KEY'] = APP_CONFIG[:secret_key]
+end
