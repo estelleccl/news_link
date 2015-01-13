8b63e15f7789120b1e84e929caaad94835261c34
Esu Chang <Esu@Esus-M.local>
2015-01-13 11:58:37 +0800

    added & configuring for devise - normal sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+
+  root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
